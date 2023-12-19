05dc6eaaa5c21e15088f314f8dfd788fd5eb05bc
Rominavila47 <rominavila656@gmail.com>
2023-12-19 16:20:00 -0300

    start mission5 .ejs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const methodOverride = require('method-override');
+/*
+const {initSession} = require('./src/util/sessions');
+requiere('dotenv').config();*/
 
 /*Routes Imports*/
 const mainRoutes = require('./src/routes/main.routes');
@@ -8,10 +12,14 @@ const authRoutes = require('./src/routes/auth.routes');
 const shopRoutes = require('./src/routes/shop.routes');
 const adminRoutes = require('./src/routes/admin.routes');
 
-const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 
  /* Define carpeta de archivos estáticos */
-app.use(express.static('public'));
+ app.use(express.static('public'));
+
+/* Configuración del template Engine - EJS */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
 
 /*Parsea los datos recibidos por Post */
 app.use(express.urlencoded());
--- src/controllers/shop.controllers.js
+++ src/controllers/shop.controllers.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    getShop: (req, res) => res.send('Esta es la vista de SHOP'),
+    getShop: (req, res) => {
+        res.render(path.resolve(__dirname, '../views/shop/shop.ejs'));        
+    },
     getShopItem: (req, res) => res.send('Esta es la vista de ITEM SELECCIONADO'),
     addItem: (req, res) => res.send('Esta es la ruta para AGREGAR un NUEVO ITEM'),
     car: (req, res) => res.send('Esta es la vista del CARRITO'),
